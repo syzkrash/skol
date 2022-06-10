e7be38d095e96ff0ce2be4611a92a7f30d78c985
qeaml <qeaml@protonmail.com>
2022-06-10 22:41:40 +0200

    python: Fix env

--- python/env.go
+++ python/env.go
@@ -2,31 +2,26 @@ package python
 
 import (
 	"github.com/syzkrash/skol/parser"
-	"github.com/syzkrash/skol/parser/nodes"
 	"github.com/syzkrash/skol/parser/values"
 )
 
 func (p *pythonState) initEnv() {
-	newScope := &parser.Scope{
-		Parent: p.parser.Scope,
-		Funcs: map[string]*parser.Function{
-			"print": {
-				Name: "print",
-				Args: map[string]values.ValueType{
-					"a": values.VtAny,
-				},
-				Ret: values.VtNothing,
+	newScope := parser.NewScope(p.parser.Scope)
+	newScope.Funcs = map[string]*parser.Function{
+		"print": {
+			Name: "print",
+			Args: map[string]values.ValueType{
+				"a": values.VtAny,
 			},
-			"import": {
-				Name: "import",
-				Args: map[string]values.ValueType{
-					"module": values.VtString,
-				},
-				Ret: values.VtNothing,
-			},
+			Ret: values.VtNothing,
 		},
-		Vars: map[string]*nodes.VarDefNode{},
+		"import": {
+			Name: "import",
+			Args: map[string]values.ValueType{
+				"module": values.VtString,
+			},
+			Ret: values.VtNothing,
+		},
 	}
-
 	p.parser.Scope = newScope
 }
